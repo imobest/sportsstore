da32f49d1cbfc13e3c4ce2ac61b8c35157e79a04
imobest <jakublenz7@gmail.com>
2021-06-07 16:06:52 +0200

    create repo with SportStore app
    SportStore web application with vue.js, vuex, routing, etc.

---
+++ src/components/Store.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col bg-dark text-white">
+                <a class="navbar-brand">SKLEP SPORTOWY</a>
+                <cart-summary />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3 bg-info p-2">
+                <CategoryControls class="mb-5" />
+                <button class="btn btn-block btn-warning mt-5"
+                v-on:click="setShowSearch(true)">
+                Szukaj
+                </button>
+            </div>
+            <div class="col-9 p-2">
+                <Search />
+                <product-list />
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import ProductList from "./ProductList";
+import CategoryControls from "./CategoryControls";
+import CartSummary from './CartSummary.vue';
+import { mapMutations } from "vuex";
+import Search from "./Search";
+export default {
+    components: { ProductList, CategoryControls, CartSummary, Search },
+    methods: {
+        ...mapMutations(["setShowSearch"])
+    }
+}
+</script>
